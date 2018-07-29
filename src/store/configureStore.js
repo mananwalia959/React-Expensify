c0cb9e45c3fcd51c41bb9ab86dd20c9112be4ae6
mananwalia959 <mananwalia959@gmail.com>
2018-07-29 11:54:05 +0530

    Fixed redux devtools (typo corrected)

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,7 @@ import {createStore,combineReducers,applyMiddleware, compose} from 'redux';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
 import thunk from 'redux-thunk'
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_Compose__||compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__||compose;
 
 export default () =>{
     const store =createStore(
